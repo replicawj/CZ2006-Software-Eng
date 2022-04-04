d7aa77bbcc5342a67a65c064e3c5121c0d3f9f0a
lyzx2001 <98450799+lyzx2001@users.noreply.github.com>
2022-04-04 16:09:12 +0800

    Update formValidator.js

--- static/assets/build/formValidator.js
+++ static/assets/build/formValidator.js
@@ -1,26 +1,21 @@
-/**
- *  onsubmit="validateForm()"
- 
- else {
-  	alert(name);
-  	db.collection('accounts').add({ //adds a document into the collection 'accounts'
-        email: name,
-        password: password
-    });
-
-function validateForm() {
-	let email = document.forms["addAccount"]["email"].value;
-	let password = document.forms["addAccount"]["password"].value
-
- */
- 
-const data = [[1, 2], [3, 4],[3, 4]];
+var data = [];
+// console.log(data);
 
 function loadInArray(doc){
 	let email = doc.data().email;
 	let password = doc.data().password;
 	data.push([email, password]);
+	//console.log(data);
 	//console.log(email+password);
+}
+
+function checkEmailExists(email){
+	for (var i = 0; i < data.length; i++) {
+		if (data[i][0] == email) {
+			return true;
+		}
+	}
+	return false;
 }
 
 db.collection('accounts').get().then((snapshot) => { //get all documents from 'accounts' collection
@@ -28,43 +23,44 @@ db.collection('accounts').get().then((snapshot) => { //get all documents from 'a
 			loadInArray(doc);
 		})
 	})
-	
-console.log(data);
-console.log(data.length);
-data.push([555, 561]);
-console.log(data);
-    
+
+
 const form = document.forms["addAccount"]; //form reference
-// saving data (add new document under account) - listen for 'submit' buttom.
+
 form.addEventListener('submit', (e) => {
     e.preventDefault(); //remove default because default will reload the page.
-    
+
+    var email = form.floatingInput.value;
+    var password = form.floatingPassword.value;
+    var verifyPassword = form.floatingVerifyPassword.value;
+
+
     if (email == "" && password == "") {
-		alert("Please fill in the details");
-		return false;
-		
-		} else if (email == ""){
-		alert("Email must be filled out");	
-		
-		} else if(password == ""){
-			alert("Password must be filled out");
-		
-		} else if (form.floatingPassword.value != form.floatingVerifyPassword.value){
-		alert("Password does not match");
-	
+        alert("Please fill in the details");
+        return false;
+
+        } else if (email == ""){
+        alert("Email must be filled out");
+
+        } else if(password == ""){
+            alert("Password must be filled out");
+
+        } else if (form.floatingPassword.value != form.floatingVerifyPassword.value){
+        alert("Password does not match");
+
+    } else if (checkEmailExists(email)) {
+		alert("Account already exists");
 	} else {
-		db.collection('accounts').add({ //adds a document into the collection 'accounts'
+        db.collection('accounts').add({ //adds a document into the collection 'accounts'
         email: form.floatingInput.value,
         password: form.floatingPassword.value
     });
-    
+
     form.floatingInput.value = ''; //clear the form after submit
     form.floatingPassword.value = '';
     form.floatingVerifyPassword.value = '';
     alert("Account Created!");
-    
-	}
-});
-  
-//db collection works but not in a function?
+
+    }
+}); 
 

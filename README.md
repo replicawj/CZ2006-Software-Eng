67d70bdf24c2a0ab30866fc8007bd99244e6f746
wxpei <wxpei_7699@hotmail.com>
2022-04-05 16:05:29 +0800

    Update all

---
+++ README.md
@@ -0,0 +1,15 @@
+# CZ2006: Software Engineering
+__Project__: Travello, a web application that have functionalities for travellers coming to Singapore
+In this project I use Python, Bootstrap and Flask to create a web application and deploy it to the cloud using Google App Engine
+
+# Technology
+- Python
+- Flask
+- HTML
+- CSS
+- Bootstrap
+- Google App Engine
+- Folium
+- Itsdangerous
+- Numpy
+- Pandas
---
+++ static/javascript/login.js
@@ -0,0 +1,58 @@
+/* Script includes: Load from database, and login handler.
+First part of script loads database from firebase.
+*/
+
+var data = [];
+// console.log(data);
+
+function loadInArray(doc){ //load database into array.
+	let email = doc.data().email;
+	let password = doc.data().password;
+	data.push([email, password]);
+	//console.log(data);
+	//console.log(email+password);
+}
+
+db.collection('accounts').get().then((snapshot) => { //get all documents from 'accounts' collection
+    snapshot.docs.forEach(doc =>  {
+        loadInArray(doc);
+    })
+})
+
+/*
+Login handler below
+*/
+
+function checkAccountExists(email,password){
+	for (var i = 0; i < data.length; i++) {
+		if (data[i][0] == email) {
+			console.log(data[i][0]);
+			if(data[i][1] == password){
+				return true;
+			}
+			return false;
+		}
+	}
+	return false;
+}
+
+
+const form = document.forms["login"]; //form reference
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); //remove default because default will reload the page.
+
+    var email = form.floatingInput.value;
+    var password = form.floatingPassword.value;
+
+    if (checkAccountExists(email,password)) {
+		alert("Login!");
+		location.replace("/flights"); //to be replaced with home page.
+	} else {
+        alert("Incorrect password or username.");
+    }
+
+    form.floatingInput.value = ''; //clear the form after submit
+    form.floatingPassword.value = '';
+
+});

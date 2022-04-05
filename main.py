4965f99fef0a33981334ad669e68dff8963060bc
wxpei <wxpei_7699@hotmail.com>
2022-04-05 17:49:54 +0800

    Revert "Update for transport"
    This reverts commit 2f9e7fe914fbb6844717b3b58b8be6856ee27b53.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import requests
 import folium
+import json
 from datetime import datetime
 from flask import Flask, render_template, request
 
-### MINE ###
+### Flights API -wj ###
 def flightsPage():
     url = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v2/prices/nearest-places-matrix"
 
@@ -25,18 +26,28 @@ def flightsPage():
     print(data['prices'])
     data = data['prices']
 
-
-
     return(data)
 
 flightsData = flightsPage()
+
+### Hotel API -wj ###
+def hotelRecco():
+    f = open('hotelInfo.json')
+
+    data = json.load(f)
+
+    return (data)
+
+hotelReccoCard = hotelRecco()
+
+
 
 
 app = Flask(__name__)
 
 @app.route('/') ##landing page
 def home():
-    return render_template("attractions.html")
+    return render_template("home.html")
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -53,35 +64,55 @@ def signup():
 
 @app.route('/attractions')
 def attractions():
-    return render_template("attractions.html")
+    no_adults = [0,1,2,3,4,5,6,7,8,9,10]
+    no_children = [0,1,2,3,4,5,6,7,8,9,10]
+    
+    return render_template("attractions.html", no_adults = no_adults, no_children = no_children)
+
+@app.route('/attractionsResult')
+def attractionsResult():
+    no_adults = [0,1,2,3,4,5,6,7,8,9,10]
+    no_children = [0,1,2,3,4,5,6,7,8,9,10]
+    return render_template("attractionsResult.html", no_adults = no_adults, no_children = no_children)
 
 @app.route('/flights')
 def flights():
     flight_class = ["Economy", "Business", "First Class"]
-    no_travellers = ["Travellers:",1,2,3,4,5,6,7,8,9,10]
+    no_travellers = ["Travellers:",0,1,2,3,4,5,6,7,8,9,10]
     return render_template("flights.html", flight_class = flight_class, no_travellers = no_travellers, result = flightsData)
 
 @app.route('/flightsResult')
 def flightsResult():
-    return render_template("flightsResult.html", result = flightsData)
+    flight_class = ["Economy", "Business", "First Class"]
+    no_travellers = ["Travellers:",1,2,3,4,5,6,7,8,9,10]
+    return render_template("flightsResult.html", flight_class = flight_class, no_travellers = no_travellers, result = flightsData)
+    # return render_template("flightsResult.html", result = flightsData)
 
 @app.route('/hotels')
 def hotels():
-    numbers = [0,1,2,3,4,5,6,7,8,9,10]
-    return render_template("hotels.html", numbers=numbers)
+    no_adults = [0,1,2,3,4,5,6,7,8,9,10]
+    no_children = [0,1,2,3,4,5,6,7,8,9,10]
+    
+    return render_template("hotels.html",no_adults = no_adults, no_children = no_children, result = hotelReccoCard)
 
 @app.route('/hotelsResult')
 def hotelsResult():
-    return render_template("hotelsResult.html")
+    no_adults = [0,1,2,3,4,5,6,7,8,9,10]
+    no_children = [0,1,2,3,4,5,6,7,8,9,10]
+    return render_template("hotelsResult.html",no_adults = no_adults, no_children = no_children, result = hotelReccoCard)
 
 @app.route('/transport')
 def transport():
-    transportMode = ["Mode of Transport:", "driving", "walking", "bicycling", "transit", "flying"]
+    transportMode = ['Mode of Transport:', 'Car', 'Bus', "Walk", "Taxi"]
     return render_template("transport.html", transportMode=transportMode)
 
 @app.route('/account')
 def account():
     return render_template("account.html")
+
+@app.route('/plans')
+def plans():
+    return render_template("plans.html")
 
 @app.route('/settings')
 def settings():

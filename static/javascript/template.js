a18bed0f9348e79efcbc570f6b1c89f3bddd1aac
wxpei <wxpei_7699@hotmail.com>
2022-04-07 16:46:42 +0800

    Update template.js
    added back the signup and login button on top of the template.
    If detected login it will be the user profile pic can click to go account.

--- static/javascript/template.js
+++ static/javascript/template.js
@@ -1,11 +1,21 @@
-if (sessionStorage.getItem("email") == null){
+if (sessionStorage.getItem("email") == null){ //not logged in
     sessionStorage.setItem("loggedIn", false);
+    //document.getElementById("loginButton").innerHTML = 
+    //display_image();
+    //alert("here");
+    //show_image("static/assets/img/userIcon.png",64,64,"profilePic");
+    //imageHere("static/assets/img/userIcon.png",64,64,"profilePic");
 
-} else {
+} else { //logged in
     sessionStorage.setItem("loggedIn", true);
     document.getElementById("signupButton").innerHTML = null;
     document.getElementById("loginButton").innerHTML = null;
+
+    //change login button to display user profile pic.
+    document.getElementById("loginButton").innerHTML = "<img id=\"profilePic\" src=\"static/assets/img/userIcon.png/\" style=\"width:60px;height:60px;\"  onclick=\"goToAccount()\"> ";
+    
 }
 
-
-//alert(sessionStorage.getItem("loggedIn"));
+function goToAccount(){
+    document.location.href = "/account";
+}
